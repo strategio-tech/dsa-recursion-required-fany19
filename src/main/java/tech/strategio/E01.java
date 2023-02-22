4e7dcd6c870ae6b9a3ba3e982b75ff51a579ccb2
estephanysanchez <estephany.sanchezcriado@strategio.tech>
2023-02-22 10:24:05 -0500

    Solved

--- src/main/java/tech/strategio/E01.java
+++ src/main/java/tech/strategio/E01.java
@@ -4,7 +4,14 @@ import java.util.*;
 
 public class E01 {
     public int getNthFib(int n) {
-        // FIXME
-        return -1;
+        if(n==1 || n==0)
+            return 0;
+        if(n==2)
+            return 1;
+        if(n<1)
+            return n;
+
+        return getNthFib(n-1)+getNthFib(n-2);
     }
+
 }
